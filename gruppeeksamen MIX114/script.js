a96ca7d7abb905667e6dfcc48ad094eb975761d9
Johannegul <jogul5674@uib.no>
2023-05-08 13:47:37 +0200

    punkter

--- gruppeeksamen MIX114/script.js
+++ gruppeeksamen MIX114/script.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
   var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth'
+    initialView: 'dayGridMonth',
+    navLinks: true,
+    dayMaxEvents: true,
+    events: [
+    {
+      title: ' ',
+      start: '2023-05-12T10:30:00',
+      end: '2023-05-12T12:30:00'
+    }
+    ]
   });
   calendar.render();
 });
