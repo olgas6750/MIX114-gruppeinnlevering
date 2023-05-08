42c518774cfe94fbe8c36da5de8afe387bda8a6c
olgas6750 <olgas6750@uib.no>
2023-05-08 10:15:44 +0200

    added packages now show up in the main container

--- gruppeeksamen MIX114/addOrderScript.js
+++ gruppeeksamen MIX114/addOrderScript.js
@@ -1,19 +1,31 @@
-let packages = {};
+let orders = {};
+let packageNumber = 0;
 
 function addPackage(){
+    packageNumber++;
     //defines a couple variables that are used in the function
     let packageList = document.getElementById("sideBarPackageList");
-    let listLength = packageList.getElementsByTagName("li").length;
-    let packageNumber = listLength;
 
-    packages
-
+    // creates a new button in the list with the corresponding number
     let newButton =
-    '<li>'+
-    '<button class="viewPackageButton" onclick="viewPackage('+packageNumber+')">'+
-    'New package' + packageNumber +
-    '</button>'+
-    '</li>';
+    '<a '+
+    'class="list-group-item list-group-item-action"'+
+    'href="#list-item-'+packageNumber+'"'+
+    '>Package '+packageNumber+'</a'+
+    '>';
 
-    packageList.innerHTML = newButton + packageList.innerHTML;
+    packageList.innerHTML += newButton;
+
+    let packages = document.getElementById("mainContainerPackageList");
+    
+    let newPackageInfo = 
+    '<h1 id="list-item-'+packageNumber+'">Package '+packageNumber+'</h1>'
+
+    packages.innerHTML += newPackageInfo;
+
+    if(packageNumber == 1){
+        let image = document.getElementById("addPackageImage");
+        image.parentNode.removeChild(image);
+    }
+
 };
