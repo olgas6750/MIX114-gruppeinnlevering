70c5e752fe7e92883da4cc5811acdb14a33b1424
olgas6750 <olgas6750@uib.no>
2023-05-13 19:07:47 +0200

    working on getting the modal to show the right info

--- gruppeeksamen MIX114/overview.js
+++ gruppeeksamen MIX114/overview.js
@@ -1,4 +1,10 @@
+window.onload = async function(){
+  const url = "https://api.npoint.io/22c58a328e7e75e91929";
 
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+};
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
@@ -20,21 +26,14 @@ document.addEventListener('DOMContentLoaded', function() {
         end: '2023-05-15T22:30:00'
       }
     ],
+
     eventClick: function(info) {
-      // info.event holds information about the clicked event
-      // display the event details in the overlay here
-      var title = info.event.title;
-      var description = info.event.extendedProps.description;
-      document.getElementById('eventOverlayTitle').textContent = title;
-      document.getElementById('eventOverlayDescription').textContent = description;
-      document.getElementById('eventOverlay').style.display = 'block';
-    }
+      loadModal(info);
+    } 
   });
-
-  // close the overlay when the close button is clicked
-  document.getElementById('eventOverlayCloseButton').addEventListener('click', function() {
-    document.getElementById('eventOverlay').style.display = 'none';
-  });
-
   calendar.render();
 });
+
+function loadModal(){
+  $('#orderModal').modal('show');
+}
