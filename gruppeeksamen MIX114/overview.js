337feb992f9eef181c2ad7a5054eab6047807fbc
ArneBHuset <Arne.Hustveit@student.uib.no>
2023-05-14 17:15:29 +0200

    maps API launch

--- gruppeeksamen MIX114/overview.js
+++ gruppeeksamen MIX114/overview.js
@@ -1,74 +1,82 @@
-const userName = "Oliver Hansen";
+// HERE STARTS OVERVIEW MAP
+async function initMap() {
+     const mapOptions = {
+          center: { lat: 60.389181, lng: 5.333219 },
+          zoom: 14,
+     };
 
-document.addEventListener('DOMContentLoaded', function() {
-  var calendarEl = document.getElementById('calendar');
-
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth',
-    navLinks: true,
-    dayMaxEvents: true,
-
-    eventClick: function(info) {
-      loadModal(info);
-    } 
-  });
-    calendar.render();
-    loadEvents(calendar);
-  }
-);
-
-async function loadEvents(calendar){
-  const url = "https://api.npoint.io/22c58a328e7e75e91929";
-
-  const response = await fetch(url);
-  data = await response.json();
-  console.log(data);
-
-  var orderCount = Object.keys(data.orders).length;
-
-  
-  for(let i = 0; i<orderCount; i++){
-    let currentOrder = data.orders[i];
-    let orderID = data.orders[i].order_number;
-    let currentPackageList = data.orders[i].packages;
-    console.log(currentPackageList);
-    
-    if(currentOrder.sender.name == userName)
-    {
-      const pickUpDate = currentOrder.pickup_date;
-      
-      var event = {
-        id: orderID,
-        title: "Order pickup",
-        start: pickUpDate,
-        color: "blue",
-        extendedProps: currentOrder
-      }
-
-      calendar.addEvent(event);
-    }
-    else if(currentOrder.recipient.name == userName)
-    { 
-      const deliveryDate = currentOrder.delivery_date;
-      
-      var event = {
-        id: orderID,
-        title: "Order delivery",
-        start: deliveryDate,
-        color: "green",
-        extendedProps: currentOrder
-      }
-
-      calendar.addEvent(event);
-    }
-    else
-    {
-      alert("error hehe");
-    }
-  }
+     mapOverview = new google.maps.Map(
+          document.getElementById("overview-modal-goolgeMaps"),
+          mapOptions
+     );
 }
 
-function loadModal(info){
-  console.log(info);
-  $('#orderModal').modal('show');
+// HERE ENDS OVERVIEW MAP
+
+const userName = "Oliver Hansen";
+
+document.addEventListener("DOMContentLoaded", function () {
+     var calendarEl = document.getElementById("calendar");
+
+     var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: "dayGridMonth",
+          navLinks: true,
+          dayMaxEvents: true,
+
+          eventClick: function (info) {
+               loadModal(info);
+          },
+     });
+     calendar.render();
+     loadEvents(calendar);
+});
+
+async function loadEvents(calendar) {
+     const url = "https://api.npoint.io/22c58a328e7e75e91929";
+
+     const response = await fetch(url);
+     data = await response.json();
+     console.log(data);
+
+     var orderCount = Object.keys(data.orders).length;
+
+     for (let i = 0; i < orderCount; i++) {
+          let currentOrder = data.orders[i];
+          let orderID = data.orders[i].order_number;
+          let currentPackageList = data.orders[i].packages;
+          console.log(currentPackageList);
+
+          if (currentOrder.sender.name == userName) {
+               const pickUpDate = currentOrder.pickup_date;
+
+               var event = {
+                    id: orderID,
+                    title: "Order pickup",
+                    start: pickUpDate,
+                    color: "blue",
+                    extendedProps: currentOrder,
+               };
+
+               calendar.addEvent(event);
+          } else if (currentOrder.recipient.name == userName) {
+               const deliveryDate = currentOrder.delivery_date;
+
+               var event = {
+                    id: orderID,
+                    title: "Order delivery",
+                    start: deliveryDate,
+                    color: "green",
+                    extendedProps: currentOrder,
+               };
+
+               calendar.addEvent(event);
+          } else {
+               alert("error hehe");
+          }
+     }
+}
+
+function loadModal(info) {
+     console.log(info);
+     $("#orderModal").modal("show");
 }
