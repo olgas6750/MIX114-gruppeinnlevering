18e7369f3d6982bb0def82f3fcfc95e0cc232dd2
ArneBHuset <Arne.Hustveit@student.uib.no>
2023-05-15 18:39:22 +0200

    Revert "pushy"
    This reverts commit 41dac5ccefe71b14c93b98dbce36836bafda4c01.

--- gruppeeksamen MIX114/JS Delivery - page 1.js
+++ gruppeeksamen MIX114/JS Delivery - page 1.js
@@ -6,10 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
           navLinks: true,
           dayMaxEvents: true,
           allDaySlot: false,
+
+          eventClick: function (info) {
+               loadModal(info);
+          },
      });
      calendar.render();
-
-     let ordersData = [];
 
      const acceptButton = document.getElementById("delivery1-acceptButton");
      const declineButton = document.getElementById("delivery1-declineButton");
@@ -64,7 +66,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     "https://api.npoint.io/1a82a1d24a67d58b1354"
                );
                const data = await response.json();
-               ordersData = data.orders;
+               console.log(data); // Check your fetched data structure
 
                // Create buttons for each order
                for (let index = 0; index < data.orders.length; index++) {
@@ -115,25 +117,9 @@ document.addEventListener("DOMContentLoaded", function () {
                       </div>
                   `;
 
+                    // Add click event listener to the button
                     btn.addEventListener("click", () => {
                          console.log(order);
-                         if (btn.getAttribute("aria-pressed") == "true") {
-                              const event = {
-                                   title: order.sender.name,
-                                   start: order.pickup_date,
-                              };
-                              calendar.addEvent(event);
-                         } else {
-                              const events = calendar.getEvents();
-                              const eventToDelete = events.find(
-                                   (e) =>
-                                        e.startStr === order.pickup_date &&
-                                        e.title === order.sender.name
-                              );
-                              if (eventToDelete) {
-                                   eventToDelete.remove();
-                              }
-                         }
 
                          // Get the parent element
                          const parentElement = document.getElementById(
